733962f096eebbe9015d5987763412f094a6a4ad
Raff <archangel.raffael@gmail.com>
2022-02-06 16:30:12 -0500

    counter storage

--- include/storage.h
+++ include/storage.h
@@ -7,9 +7,12 @@
 #define GT_MEM_SIZE_SSID 40
 #define GT_MEM_ADDR_PASS 40
 #define GT_MEM_SIZE_PASS 40
+#define GT_MEM_ADDR_CNTR 80
 
 void initialize_eeprom();
 void gt_mem_get_ssid(char *out);
 void gt_mem_get_pass(char *out);
 void gt_mem_set_ssid(char *new_ssid);
 void gt_mem_set_pass(char *new_pass);
+uint32_t gt_mem_get_cntr();
+void gt_mem_set_cntr(uint32_t new_cntr);
